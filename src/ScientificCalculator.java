6245742b31aabf333bfd8e19c099e38e4f5bfda5
lissakariuki <melgaka1106@gmail.com>
2024-03-08 23:58:58 +0300

    Setting the buttons to true

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -40,6 +40,7 @@ public class ScientificCalculator extends javax.swing.JFrame {
        jButton16.setEnabled(true);
        jButton17.setEnabled(true);
        jButton18.setEnabled(true);
+       jButton19.setEnabled(true);
     }
     
     public void disable()
