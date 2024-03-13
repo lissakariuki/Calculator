56220d43083fd113d8e66d1759be057e5c581a5f
lissakariuki <melgaka1106@gmail.com>
2024-03-13 20:00:49 +0300

    multiplication

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -477,7 +477,11 @@ public class ScientificCalculator extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        TextField.setText(TextField.getText() + " * ");
+            num = Double.parseDouble(TextField.getText());
+            calculation = 1;
+            TextField.setText("");
+            jLabel1.setText(num + "+");
+            //TextField.setText(TextField.getText() + " * ");
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
